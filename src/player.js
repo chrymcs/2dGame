da770c748175da4e9069a8f8afd8f4d8a3baa17c
chrymcs <chrymcs@gmail.com>
2022-10-12 07:45:08 +0300

    vertical movement (jump)

--- src/player.js
+++ src/player.js
@@ -7,6 +7,8 @@ export class Player {
         this.y = this.game.height - this.height;
         this.image = document.getElementById('player');
         this.speed = 0;
+        this.vy = 0;
+        this.weight = 1;
     }
     update(input) {
         // horizontal movement
@@ -17,9 +19,23 @@ export class Player {
         // prevent from exceeding canvas boundaries
         if (this.x < 0) this.x = 0;
         if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
+        
+        // vertical movement (jump)
+        // i want player to be able to jump only when he's on the ground.
+        if (input.includes('w') && this.onGround()) this.vy -= 30;
+        this.y += this.vy;
+        if (!this.onGround()) this.vy += this.weight;
+        else this.vy = 0;
+        // prevent from exceeding canvas boundaries
+        //if (this.y < 0) this.y = 0;
+        if (this.y > this.game.height - this.height) this.y = this.game.height - this.height;
+        
     }
     draw(context){
         context.drawImage(this.image, 0, 0, this.width, this.height, 
             this.x, this.y, this.width, this.height);
+    }
+    onGround(){
+        return this.y >= this.game.height - this.height;
     }
 }
